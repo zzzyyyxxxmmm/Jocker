d99be38069980f7b5187bcdd2a06c08c62f5099a
Jikang Wang <wjk32111@gmail.com>
2020-01-09 15:22:25 -0800

    add busybox

--- boot/init.go
+++ boot/init.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -16,9 +17,7 @@ func RunContainerInitProcess() error {
 	if cmdArray==nil || len(cmdArray)==0 {
 		return fmt.Errorf("Run container get user command error, cmdArray is nil")
 	}
-
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	setUpMount()
 	path, err:=exec.LookPath(cmdArray[0])
 	if err!=nil{
 		log.Errorf("Exec loop path error %v", err)
@@ -61,6 +60,7 @@ func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
 		cmd.Stderr = os.Stderr
 	}
 	cmd.ExtraFiles=[]*os.File{readPipe}
+	cmd.Dir="/root/busybox"
 	return cmd, writePipe
 }
 
@@ -72,3 +72,46 @@ func NewPipe() (*os.File, *os.File, error) {
 	return read, write, nil
 }
 
+func setUpMount(){
+	pwd, err:=os.Getwd()
+
+	if err!=nil{
+		log.Errorf("Get current location error %v", err)
+	}
+
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+
+	defaultMountFlags:=syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags),"")
+
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME,"mode=755")
+}
+
+func pivotRoot (root string) error {
+	if err:=syscall.Mount(root, root, "bind", syscall.MS_BIND | syscall.MS_REC, ""); err!=nil{
+		return fmt.Errorf("Mount rootfs to itself error: %v",err)
+	}
+
+	pivotDir:=filepath.Join(root, ".pivot_root")
+
+	if err:=os.Mkdir(pivotDir, 0777); err!=nil{
+		return err
+	}
+
+	if err:=syscall.PivotRoot(root,pivotDir);err!=nil{
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	if err:=syscall.Chdir("/");err!=nil{
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir= filepath.Join("/",".pivot_root")
+
+	if err:=syscall.Unmount(pivotDir, syscall.MNT_DETACH); err!=nil{
+		return fmt.Errorf("unmount pivot_root dir %v",err)
+	}
+	return os.Remove(pivotDir)
+}
